ad8324b527cc07a8f6200c9a140a944f68552f93
leonorden <39034760+leonorden@users.noreply.github.com>
2023-10-25 03:22:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -32,7 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
       el.style.setProperty("--bg-x", bgX + "%");
       el.style.setProperty("--bg-y", bgY + "%");
       el.style.setProperty("--r-x", rX + "deg");
-      el.style.setProperty("--r-y", rY + "deg");    
+      el.style.setProperty("--r-y", rY + "deg");
+
+      // Show layer2 when tilted
+      showLayer2();
+    } else {
+      // Hide layer2 when not tilted
+      hideLayer2();
     }
   });
 
@@ -44,6 +50,19 @@ document.addEventListener("DOMContentLoaded", function () {
     el.style.setProperty("--bg-y", "50%");
     el.style.setProperty("--r-x", "0deg");
     el.style.setProperty("--r-y", "0deg");
+
+    // Hide layer2 when not tilted
+    hideLayer2();
+  }
+
+  function showLayer2() {
+    const layer2 = document.querySelector(".card__layer2");
+    layer2.style.visibility = "visible";
+  }
+
+  function hideLayer2() {
+    const layer2 = document.querySelector(".card__layer2");
+    layer2.style.visibility = "hidden";
   }
 
   // Function to start the shaking animation
