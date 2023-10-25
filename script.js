ca843ef03788bf3bd8b1932ec594911791a05104
leonorden <39034760+leonorden@users.noreply.github.com>
2023-10-25 13:39:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
- 
-  const el = document.querySelector(".card");
-  let w = el.clientWidth;
-  let h = el.clientHeight;
-  let b = el.getBoundingClientRect();
+$(document).ready(function() {
+
+  const $el = $(".card");
+  const $cardLayerProfile = $(".card__layerProfile");
+
+  let w = $el.width();
+  let h = $el.height();
+  let b = $el[0].getBoundingClientRect();
   let isHovering = false;
 
-  el.addEventListener("mouseenter", () => {
+  $el.on("mouseenter", function() {
     isHovering = true;
   });
 
-  el.addEventListener("mouseleave", () => {
+  $el.on("mouseleave", function() {
     isHovering = false;
     resetTilt();
   });
 
-  el.addEventListener("mousemove", (e) => {
+  $el.on("mousemove", function(e) {
     if (isHovering) {
       let X = (e.clientX - b.left) / w;
       let Y = (e.clientY - b.top) / h;
@@ -27,59 +29,47 @@ document.addEventListener("DOMContentLoaded", function () {
       let bgY = 40 + 20 * Y;
 
       // Set the card's style properties
-      el.style.setProperty("--x", 100 * X + "%");
-      el.style.setProperty("--y", 100 * Y + "%");
-      el.style.setProperty("--bg-x", bgX + "%");
-      el.style.setProperty("--bg-y", bgY + "%");
-      el.style.setProperty("--r-x", rX + "deg");
-      el.style.setProperty("--r-y", rY + "deg");    
+      $el.css("--x", 100 * X + "%");
+      $el.css("--y", 100 * Y + "%");
+      $el.css("--bg-x", bgX + "%");
+      $el.css("--bg-y", bgY + "%");
+      $el.css("--r-x", rX + "deg");
+      $el.css("--r-y", rY + "deg");
     }
   });
 
   function resetTilt() {
     // Reset the card's style properties to their default values
-    el.style.setProperty("--x", "50%");
-    el.style.setProperty("--y", "50%");
-    el.style.setProperty("--bg-x", "50%");
-    el.style.setProperty("--bg-y", "50%");
-    el.style.setProperty("--r-x", "0deg");
-    el.style.setProperty("--r-y", "0deg");
+    $el.css("--x", "50%");
+    $el.css("--y", "50%");
+    $el.css("--bg-x", "50%");
+    $el.css("--bg-y", "50%");
+    $el.css("--r-x", "0deg");
+    $el.css("--r-y", "0deg");
   }
 
-  // Function to start the shaking animation
-  function startShaking() {
-    const cardLayerProfile = document.querySelector(".card__layerProfile");
-    cardLayerProfile.classList.add("shaking");
-    setTimeout(() => {
-      stopShaking(); // Pause and reset the animation
-      setTimeout(() => {
-        startShaking(); // Start the animation again after 10 seconds
-      }, 10000);
-    }, 5000); // Pause after 5 seconds
+  function startSwinging() {
+    $cardLayerProfile.css("animation", "swing 5s ease infinite");
   }
 
-  // Function to start the interval
+  function stopSwinging() {
+    $cardLayerProfile.css("animation", "none");
+    $cardLayerProfile.css("transform", "translateX(0)"); // Reset the transform property
+  }
+  
   function startAnimationCycle() {
-    startShaking(); // Start the animation initially
-
-    setInterval(() => {
-      stopShaking(); // Pause and reset the animation
-      setTimeout(() => {
-        startShaking(); // Start the animation again after 5 seconds
-      }, 5000);
-    }, 10000); // Repeat the cycle every 10 seconds (5 seconds of shaking and 5 seconds of pause)
+    startSwinging();
+    setTimeout(function() {
+      stopSwinging();
+      setTimeout(startSwinging, 5000); // Start swinging again after a pause of 5 seconds
+    }, 5000); // Swing for 5 seconds, then pause for 5 seconds
   }
 
   // Start the animation cycle
   startAnimationCycle();
 
-  // Function to stop the shaking animation and reset to the original position
-    function stopShaking() {
-    const cardLayerProfile = document.querySelector(".card__layerProfile");
-    cardLayerProfile.classList.remove("shaking");
-
-    // Reset the card's position to its original state
-    cardLayerProfile.style.transform = "translateX(0)";
-  }
-
 });
+
+
+ 
+
